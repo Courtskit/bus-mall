802ae9ebd0619531e86e8e9cf1b8749fc2152a0c
Courtskit <Courtskit@gmail.com>
2020-05-12 21:26:20 -0700

    Updated table to display views and votes

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var allImages = [];
 var uniqueIndexArray = [];
 var names = [];
 var votes = [];
+var views = [];
 var totalVotes = 0;
 
 
@@ -18,7 +19,6 @@ function CreateImage(url, alt, title) {
   this.views = 0;
   allImages.push(this);
 }
-
 
 
 //appends image to ID in HTML
@@ -122,6 +122,7 @@ function makeNamesArray() {
   for (var i = 0; i < allImages.length; i++) {
     names.push(allImages[i].title);
     votes.push(allImages[i].votes);
+    views.push(allImages[i].views);
   }
   generateChart();
 }
@@ -135,6 +136,54 @@ function generateChart() {
       datasets: [{
         label: '# of Votes',
         data: votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 1
+      }, {
+        label: '# of Views',
+        data: views,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
